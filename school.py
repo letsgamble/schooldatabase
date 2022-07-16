f56de87b5158dc8737c9ba0ff42a810c78911009
Kamil Wesołowski <letsgamble92@gmail.com>
2022-07-16 17:59:44 +0200

    working on supervisor

--- school.py
+++ school.py
@@ -1,23 +1,29 @@
-school_class = str()
-subject = str()
-
-rows = [
-    ("supervisor", {"Class": school_class}),
-    ("mentor", {"Subject": subject, "Class": school_class}),
-    ("student", {"Class": school_class}, )
-]
+supervisor_list = []
+supervisor_dict = {}
+# mentor = {"Name": name, "Subject": subject, "Class": school_class}
+# student = {"Name": name, "Class": school_class}
 
 
 def user_type():
-    checkwho = input()
-    if checkwho == "supervisor":
-        print("To jest supervisor")
-    if checkwho == "mentor":
+    check_who = input()
+    if check_who == "supervisor":
+        name = input()
+        supervisor_dict["User"] = check_who
+        supervisor_dict["Name"] = name
+        supervisor_list.append(supervisor_dict)
+        check_class = input()
+        while check_class != "":
+            supervisor_dict["School_class"] = check_class
+            supervisor_list.append(supervisor_dict)
+            input()
+
+    if check_who == "mentor":
         print("To jest mentor")
-    if checkwho == "student":
+    if check_who == "student":
         print("To jest student")
-    if checkwho == "end":
+    if check_who == "end":
         print("To jest student")
 
 
 user_type()
+print("List:", supervisor_list)
